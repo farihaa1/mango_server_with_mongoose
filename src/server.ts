29be996e9797ca826768041f3af7b67f8009c95a
farihaa1 <farihaa1403@gmail.com>
2026-05-30 13:34:36 +0600

    mongoose express server set up

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,34 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import config from "./config";
 
 const app = express()
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json());
 
-app.listen(5000,()=>{
+app.get("/", (req, res) => {
+    res.send({success:true, message:"server running"});
+})
+
+app.listen(config.port, () => {
     console.log(`server running`)
 })
 
 
-function server() {
+async function server() {
+    try {
+        await mongoose.connect(config.database_url as string);
 
+        console.log("MongoDB Connected");
+
+        app.listen(5000, () => {
+            console.log("Server running on port 5000");
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 server();
