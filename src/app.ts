01c0fdfccb406a67aa22bf331c46f0eaa8d40078
farihaa1 <farihaa1403@gmail.com>
2026-06-05 17:43:09 +0600

    added appEror

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express"
+import express, { Application, NextFunction, Request, Response } from "express"
 import routes from "./routes";
 
 const app: Application = express()
@@ -10,6 +10,24 @@ app.use('/api', routes);;
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is running')
 });
+
+
+
+// Global Error Handler
+app.use(
+    (
+        error: any,
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        res.status(error.statusCode || 500).json({
+            success: false,
+            message: error.message || "Something went wrong",
+            error,
+        });
+    }
+);
 
 export default app;
 
